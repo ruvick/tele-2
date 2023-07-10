68447faf2a02bc099e1ea0774129b1827bf21b34
ruvick <rudikov.web@ya.ru>
2023-07-10 18:35:15 +0300

    Плеер

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -54,9 +54,7 @@ smotScrollElems.forEach(link => {
 		});
 	})
 });
-
-
-//dom объекты элементов контроля
+// Аудиоплеер =========================================================================================================================================================================
 var audio = document.getElementById("audio");
 var av = document.getElementById("av-tag");
 var playTime = document.getElementsByClassName("play-time")[0];
@@ -120,6 +118,7 @@ av.ontimeupdate = function () {
 	}
 	if (seconds < 10) { seconds = "0" + seconds; } playTime.innerHTML = minutes + ':' + seconds;
 	if (isPlaying) curTime.value = av.currentTime;
+
 };
 //функция для настройки громкости
 // volume.onchange = function () {
@@ -128,7 +127,6 @@ av.ontimeupdate = function () {
 // };
 //функция для установки начала воспроизведения
 curTime.onchange = function () {
-
 	av.pause(); av.currentTime = curTime.value; av.play();
 };
 //функция для вкл/выкл громкости
@@ -157,152 +155,3 @@ playBtn.addEventListener("click", (a) => {
 	}
 
 });
-
-// document.addEventListener("DOMContentLoaded", (e) => {
-
-// 	//dom объекты элементов контроля
-// 	var audio = document.getElementById("audio");
-// 	var av = document.getElementById("av-tag");
-// 	var playTime = document.getElementsByClassName("play-time")[0];
-// 	var playBtn = document.getElementsByClassName("play-btn")[0];
-// 	var curTime = document.getElementById("cur-time");
-// 	var speedBtnX1 = document.querySelector('.speed-btnx1');
-// 	var speedBtnX1_5 = document.querySelector('.speed-btnx1_5');
-// 	var speedBtnX2 = document.querySelector('.speed-btnx2');
-// 	// var volume = document.getElementById("volume");
-// 	// var speaker = document.getElementById("speaker");
-
-
-// 	function speedUp() {
-// 		audio.play();
-// 		audio.playbackRate = 1.5;
-// 	}
-
-// 	function speedHightUp() {
-// 		audio.play();
-// 		audio.playbackRate = 2;
-// 	}
-
-// 	function normalSpeed() {
-// 		audio.play();
-// 		audio.playbackRate = 1;
-// 	}
-
-// 	// function setPlaySpeed() {
-// 	// 	audio.playbackRate = 1.5;
-// 	// }
-
-// 	// function setSPlaySpeed() {
-// 	// 	audio.playbackRate = 2;
-// 	// }
-
-// 	// function getPlaySpeed() {
-// 	// 	alert(audio.playbackRate);
-// 	// }
-
-// 	// function setPlaySpeed(speed) {
-// 	// 	audio.playbackRate = speed;
-// 	// }
-
-// 	// speedBtnX1.addEventListener("click", (a) => {
-// 	// 	if (speedBtnX1) {
-// 	// 		speedBtnX1.classList.add("active");
-// 	// 		audio.playbackRate = 1.0;
-// 	// 		audio.play();
-// 	// 	}
-// 	// });
-// 	// speedBtnX1_5.addEventListener("click", (a) => {
-// 	// 	if (speedBtnX1_5) {
-// 	// 		speedBtnX1_5.classList.add("active");
-// 	// 		setPlaySpeed();
-// 	// 	}
-// 	// });
-// 	// speedBtnX2.addEventListener("click", (a) => {
-
-// 	// 	if (speedBtnX2) {
-// 	// 		speedBtnX2.classList.add("active");
-// 	// 		setSPlaySpeed();
-// 	// 	}
-// 	// 	audio.play();
-// 	// });
-
-// 	// audio.addEventListener(
-// 	// 	"ratechange",
-// 	// 	function () {
-// 	// 		//код обработки события
-// 	// 		;
-// 	// 	},
-// 	// 	false);
-// 	// 	audio.addEventListener(
-// 	// 	"ratechange",
-// 	// 	function () {
-// 	// 		//код обработки события
-// 	// 		;
-// 	// 	},
-// 	// 	false);
-
-// 	// audio.playbackRate = 2.0;
-// 	// audio.play();
-
-// 	//переменная для отслеживания воспроизведения звука
-// 	var isPlaying = false;
-
-// 	av.onloadedmetadata = function () {
-// 		curTime.max = av.duration;
-// 	};
-
-// 	//функция вывода текущего времени воспроизведения
-// 	av.ontimeupdate = function () {
-
-// 		var sec_num = av.currentTime;
-// 		var hours = Math.floor(sec_num / 3600);
-// 		var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-// 		var seconds = sec_num - (hours * 3600) - (minutes * 60);
-// 		seconds = Math.round(seconds);
-
-// 		if (hours < 10) {
-// 			hours = "0" + hours;
-// 		}
-// 		if (minutes < 10) {
-// 			minutes = "0" + minutes;
-// 		}
-// 		if (seconds < 10) { seconds = "0" + seconds; } playTime.innerHTML = minutes + ':' + seconds;
-// 		if (isPlaying) curTime.value = av.currentTime;
-// 	};
-// 	//функция для настройки громкости
-// 	// volume.onchange = function () {
-
-// 	// 	av.volume = volume.value / 10;
-// 	// };
-// 	//функция для установки начала воспроизведения
-// 	curTime.onchange = function () {
-
-// 		av.pause(); av.currentTime = curTime.value; av.play();
-// 	};
-// 	//функция для вкл/выкл громкости
-// 	// speaker.onclick = function () {
-
-// 	// 	if (volume.value == 0) {
-// 	// 		volume.value = 10; av.volume = 1;
-// 	// 	} else {
-// 	// 		volume.value = 0; av.volume = 0;
-// 	// 	}
-// 	// };
-// 	//функция для play/pause и изображения кнопки воспроизведения
-// 	playBtn.addEventListener("click", (a) => {
-
-// 		if (isPlaying) {
-// 			av.pause();
-// 			isPlaying = false;
-// 			playBtn.innerHTML = "►";
-// 			// playBtn.style.display = "block";
-// 			// playBtn.innerHTML = '<svg width="18" height="22" viewBox="0 0 18 22" fill="none"><path d="M1.00147 2.44421L15.8205 11L1.00146 19.5557L1.00147 2.44421Z" stroke="#FFAF0F" stroke-width="2"/></svg>';
-// 		}
-// 		else {
-// 			av.play();
-// 			isPlaying = true;
-// 			playBtn.innerHTML = "❚❚";
-// 		}
-
-// 	});
-// });
