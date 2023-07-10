dfd2386837f4ad93ea4cda0e40d4dbc233f58b07
asmi046 <asmi046@gmail.com>
2023-07-11 01:56:33 +0300

    Плейер

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -67,6 +67,39 @@ var speedBtnX2 = document.querySelector('.speed-btnx2');
 // var speaker = document.getElementById("speaker");
 
 // Функции переключения скорости
+function getTimePart(time) {
+	var sec_num = time;
+	var hours = Math.floor(sec_num / 3600);
+	var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	var seconds = sec_num - (hours * 3600) - (minutes * 60);
+	seconds = Math.round(seconds);
+
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10) { seconds = "0" + seconds; }
+
+	return  {"minutes":minutes, "seconds":seconds}
+}
+
+function audioTimeUpdate() {
+
+	 let cur_rez_minus = getTimePart(parseFloat(av.duration)-parseFloat(av.currentTime))
+
+	playTime.innerHTML = "-"+cur_rez_minus.minutes + ':' + cur_rez_minus.seconds;
+	if (isPlaying) curTime.value = av.currentTime;
+}
+
+function audioTimeInit() {
+	let rez = getTimePart(av.duration)
+	
+	playTime.innerHTML = "-"+rez.minutes + ':' + rez.seconds;
+	curTime.value = 0;
+}
+
 function normalPlaySpeed() {
 	av.playbackRate = 1;
 }
@@ -98,26 +131,16 @@ speedBtnX2.addEventListener("click", (a) => {
 var isPlaying = false;
 
 av.onloadedmetadata = function () {
+	console.log("Загрузка метаданных")
+	console.log(av.duration)
 	curTime.max = av.duration;
+	audioTimeInit()
 };
 
 //функция вывода текущего времени воспроизведения
 av.ontimeupdate = function () {
 
-	var sec_num = av.currentTime;
-	var hours = Math.floor(sec_num / 3600);
-	var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-	var seconds = sec_num - (hours * 3600) - (minutes * 60);
-	seconds = Math.round(seconds);
-
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	if (seconds < 10) { seconds = "0" + seconds; } playTime.innerHTML = minutes + ':' + seconds;
-	if (isPlaying) curTime.value = av.currentTime;
+	audioTimeUpdate()
 
 };
 //функция для настройки громкости
@@ -144,14 +167,18 @@ playBtn.addEventListener("click", (a) => {
 	if (isPlaying) {
 		av.pause();
 		isPlaying = false;
-		playBtn.innerHTML = "►";
+		
+		playBtn.innerHTML = "►"; // Убери как напишишь CSS
+		
 		// playBtn.style.display = "block";
 		// playBtn.innerHTML = '<svg width="18" height="22" viewBox="0 0 18 22" fill="none"><path d="M1.00147 2.44421L15.8205 11L1.00146 19.5557L1.00147 2.44421Z" stroke="#FFAF0F" stroke-width="2"/></svg>';
 	}
 	else {
 		av.play();
 		isPlaying = true;
-		playBtn.innerHTML = "❚❚";
+		playBtn.classList.toggle('paused');
+
+		playBtn.innerHTML = "❚❚"; // Убери как напишишь CSS
 	}
 
 });
